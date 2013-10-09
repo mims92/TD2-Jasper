a3b1961f2cf662d4f744135b07836874fcfb36c7
Florian <Florian@192.168.0.10>
2013-10-09 10:46:08 +0200

    GUI

---
+++ src/be/esi/alg3/gui/AuteurDto.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package be.esi.alg3.gui;
+ 
+/**
+ *
+ * @author Florian
+ */
+class AuteurDto {
+
+}
